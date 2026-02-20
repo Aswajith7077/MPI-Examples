bc7b3e9b436b4ce5725591036035a89c7afb2bf7
Anirudhan R <anirudhanr03@gmail.com>
2026-02-20 23:07:07 +0530

    Update who_am_i.c

--- who_am_i.c
+++ who_am_i.c
@@ -8,7 +8,7 @@ int main(int argc, char* argv[]){
 
 	MPI_Init(&argc,&argv);
 
-	MPI_Comm_size(MPI_COMM_WORLD,&number_of_processes);
+	MPI_Comm_size(MPI_COMM_WORLD, &number_of_processes);
 	MPI_Comm_rank(MPI_COMM_WORLD,&rank);
 
 	printf("The current process with id %d among %d processes\n", rank, number_of_processes);
